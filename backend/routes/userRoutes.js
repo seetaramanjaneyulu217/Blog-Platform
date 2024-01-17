25a6b19e02b50ebf2c23ef0cf0b5638803aa16ac
seetaramanjaneyulu217 <seetaramanjaneyulu172002@gmail.com>
2024-01-17 15:11:02 +0530

    Fixed an import error

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 // imported User signup controllers
 const { registerUser, loginUser, logoutUser } = require('../controllers/userSignupController.js')
-const loginDataValidation = require('../middlewares/loginDataVAlidation.js')
+const loginDataValidation = require('../middlewares/loginDataValidation.js')
 
 router.route('/signup').post(registerUser)
 router.route('/login').post(loginDataValidation, loginUser)
